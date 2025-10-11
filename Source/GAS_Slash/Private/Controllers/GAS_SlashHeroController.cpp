ca2df21cc9277e7c88f239d1cf8175deb14d82bb
Dmytro <dmytroch04@gmail.com>
2025-10-11 04:49:28 +0200

    Setup for Inventory Plugin
    During this arc I've implemented the correct functioning of the Inventory Plugin inside the Main GAS project. I've insured that the main Input functionality works correctly and doesn't interfere the main Control System.

--- Source/GAS_Slash/Private/Controllers/GAS_SlashHeroController.cpp
+++ Source/GAS_Slash/Private/Controllers/GAS_SlashHeroController.cpp
@@ -3,12 +3,42 @@
 
 #include "Controllers/GAS_SlashHeroController.h"
 
+#include "GameFramework/Character.h"
+
 AGAS_SlashHeroController::AGAS_SlashHeroController()
 {
-	HeroTeamID = FGenericTeamId(0);
+    HeroTeamID = FGenericTeamId(0);
+    
+    // Create the equipment component
+    EquipmentComponent = CreateDefaultSubobject<UInv_EquipmentComponent>(TEXT("EquipmentComponent"));
+}
+
+void AGAS_SlashHeroController::OnPossess(APawn* InPawn)
+{
+    Super::OnPossess(InPawn);
+    
+    // Initialize equipment component when possessing a pawn
+    if (ACharacter* OwningCharacter = Cast<ACharacter>(InPawn))
+    {
+        if (EquipmentComponent)
+        {
+            EquipmentComponent->SetOwningSkeletalMesh(OwningCharacter->GetMesh());
+            EquipmentComponent->InitializeOwner(this);
+        }
+    }
 }
 
 FGenericTeamId AGAS_SlashHeroController::GetGenericTeamId() const
 {
 	return HeroTeamID;
+}
+
+void AGAS_SlashHeroController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGAS_SlashHeroController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
 }
--- Source/GAS_Slash/Public/Controllers/GAS_SlashHeroController.h
+++ Source/GAS_Slash/Public/Controllers/GAS_SlashHeroController.h
@@ -5,11 +5,14 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "GenericTeamAgentInterface.h"
+#include "Player/Inv_PlayerController.h"
+// Add include
+#include "EquipmentManagment/Components/Inv_EquipmentComponent.h"
 #include "GAS_SlashHeroController.generated.h"
 
 
 UCLASS()
-class GAS_SLASH_API AGAS_SlashHeroController : public APlayerController, public IGenericTeamAgentInterface
+class GAS_SLASH_API AGAS_SlashHeroController : public AInv_PlayerController, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
 
@@ -18,7 +21,14 @@ public:
 	
 	virtual FGenericTeamId GetGenericTeamId() const override;
 
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+    virtual void OnPossess(APawn* InPawn) override;
+    
 private:
 	FGenericTeamId HeroTeamID;
-	
+    
+    UPROPERTY(VisibleAnywhere)
+    UInv_EquipmentComponent* EquipmentComponent;
 };
