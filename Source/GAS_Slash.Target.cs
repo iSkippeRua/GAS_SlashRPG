e6f6d7b713fcc4edf8bec724c7c127bed27e33dc
Dmytro Chesniuk <dmytroch04@gmail.com>
2026-03-18 21:34:23 +0200

    Mini Map implementation start
    Right now I am working on the Mini Map for the Player. During the playtest I found out that sometimes player do not see all Enemy Characters or Items that are dropped by the Enemies, so I've decided that it would be a good idea to add the mini map to the HUD. For now I've created the mini map itself that's using Capture Component to get a live image of the scene. Also added the mini map movement that's correspond to the character movement.
    
    Next task is to:
    1) Add marker to show player position of the character on the map
    2) Add rotation to the mini map that will follow player rotation.
    3) Add corresponding markers to show enemies or different items after they are spawned.
    4) Manage the marker deletion logic after enemy death or after the item is picked up

--- Source/GAS_Slash.Target.cs
+++ Source/GAS_Slash.Target.cs
@@ -8,8 +8,8 @@ public class GAS_SlashTarget : TargetRules
 	public GAS_SlashTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("GAS_Slash");
 	}
 }
